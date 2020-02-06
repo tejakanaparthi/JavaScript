4bde8f27522a12d34fd6bdd2c7fad75c3265ad2b
Teja <tejakanaparthi@gmail.com>
2020-02-06 18:24:12 -0500

    Code to display the day and date in 12Hour format

---
+++ date.js
@@ -0,0 +1,23 @@
+let date=new Date();
+
+const day=date.getDay();
+ 
+const dayArray=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+let hour=date.getHours();
+if(hour>12){
+  
+    hour=hour%12;
+    hour= hour+" "+ "PM"
+}
+else if(hour===24){
+ 
+    hour =12+" "+AM
+}else{
+ 
+    hour= hour+" "+"AM"
+}
+
+console.log(`Today is: ${dayArray[day]}`);
+
+console.log(`${hour}: ${date.getMinutes()}: ${date.getSeconds()}`);
